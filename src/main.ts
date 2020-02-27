9e3389561db9ba459647dfee4b1c7e2beba4648e
Kohei Hasegawa <ameutau@gmail.com>
2020-02-27 21:25:31 +0900

    Handle addColumn space

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
 import { debounce } from './utils';
 
 const waitMs = 30;
+const cssTextToShrink = "min-width: 30px;";
 
 const columns = () => document.querySelectorAll<HTMLDivElement>('.js-project-column');
 
 const callback = () => {
   const chosen = document.querySelector('.js-project-column .sortable-chosen')
   const ghost = document.querySelector('.js-project-column .sortable-ghost')
+  const addColumn = document.querySelector<HTMLDivElement>('.js-new-project-column-container')
 
   // without ghost, it would fire just click the title and can't drag it.
   if (chosen && ghost) {
     const chosenColumn = chosen?.parentElement?.parentElement;
     const ghostColumn = ghost?.parentElement?.parentElement;
 
+    // addColumn occupies a large area of land.
+    if (addColumn) addColumn.style.cssText = "display: none";
+
     columns().forEach((column) => {
       if (column !== chosenColumn && column !== ghostColumn) {
-        column.style.cssText = "min-width: 30px;";
+        column.style.cssText = cssTextToShrink;
       } else {
         column.removeAttribute('style')
       }
@@ -24,6 +29,9 @@ const callback = () => {
     columns().forEach(column => {
       column.removeAttribute('style')
     });
+    if (addColumn) {
+      addColumn.removeAttribute('style')
+    }
   }
 };
 
